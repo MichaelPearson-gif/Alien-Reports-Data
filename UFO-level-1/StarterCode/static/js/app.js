59db59e25bf0d45a51c84c6cc43e2ffc6680d904
Michael Pearson <mpearson2516@yahoo.com>
2019-10-24 21:11:41 -0700

    Cleaned up the code.

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -6,53 +6,35 @@ var tableData = data;
 // Reference he table body
 var tbody = d3.select("tbody");
 
-// // Loop through the data
-// tableData.forEach(function(UFO) {
-//     console.log(UFO);
-// });
-
-// // Append to one table row 'tr'
-// tableData.forEach(function(UFO) {
-//     console.log(UFO);
-//     var row = tbody.append("tr");
-// });
-
-// // Use Object.entries to print the ufo sightings
-// tableData.forEach(function(UFO) {
-//     console.log(UFO);
-//     var row = tbody.append("tr");
-
-//     Object.entries(UFO).forEach(function([key, value]) {
-//         console.log(key, value);
-//     });
-// });
-
-// // Use d3 to append 1 cell per ufo sighting value (date/time, city, state, country, shape, and comment)
-// tableData.forEach(function(UFO) {
-//     console.log(UFO);
-//     var row = tbody.append("tr");
-
-//     Object.entries(UFO).forEach(function([key, value]) {
-//         console.log(key, value);
-//         var cell = row.append("td");
-//     });
-// });
-
-// // Use d3 to update each cell's text
-// tableData.forEach(function(UFO) {
-//     console.log(UFO);
-//     var row = tbody.append("tr");
-//     Object.entries(UFO).forEach(function([key, value]) {
-//         console.log(key, value);
-//         var cell = row.append("td");
-//         cell.text(value);
-//     });
-// });
-
-tableData.forEach((UFO) => {
+// Use d3 to create the table and append the data to it
+tableData.forEach(function(UFO) {
+    console.log(UFO);
     var row = tbody.append("tr");
-    Object.entries(UFO).forEach(([key, value]) => {
+    Object.entries(UFO).forEach(function([key, value]) {
+        console.log(key, value);
         var cell = row.append("td");
         cell.text(value);
     });
+});
+
+// Search by date 
+
+// Select the button
+var button = d3.select("#button");
+
+button.on("click", function() {
+
+    // Select the input value
+    var inputElement = d3.select("#datetime");
+
+    // Get the value
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    // Filter the data
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    console.log(filteredData);
 });
