4233d93b8d664b6ecb5d7fdcd359427abd629f08
Michael Pearson <mpearson2516@yahoo.com>
2019-10-29 12:30:12 -0700

    Level 2 code works, just needs to be able to look at multiple parameters.

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -3,10 +3,11 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-// Reference the table body
+// Reference the table body and button tags
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 
+// Create a function to build a table
 function buildTable(data) {
     tbody.html("");
     data.forEach(function(UFO) {
@@ -18,6 +19,7 @@ function buildTable(data) {
     });
 };
 
+// Create a function that will filter the data at the press of a button
 function buttonClick() {
     var inputDate = d3.select("#datetime").property("value");
     var searchResults = tableData;
@@ -29,5 +31,6 @@ function buttonClick() {
     buildTable(searchResults);
 }
 
+// Call back to the buildTable function and buttonClick function to implement them.
 buildTable(data);
 d3.selectAll("#filter-btn").on("click",buttonClick);
